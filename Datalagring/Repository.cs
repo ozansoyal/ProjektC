c59f9c41eabdc86386de12178fa6187e44aa4234
ozansoyal <ozan_soyal@hotmail.com>
2024-10-17 11:53:48 +0200

    test

---
+++ Datalagring/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Datalagring
+{
+    public class Repository
+    {
+        private List<Podd> poddLista = new List<Podd>();
+        public void LäggTillPodd(Podd podd)
+        {
+            poddLista.Add(podd);
+        }
+        public List<Podd> HämtaAllaPoddar()
+        { return poddLista; }
+        }
+}
---
+++ Models/Podd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Models
+{
+    public class Podd
+    {
+        public string Id { get; set; }
+        public string Rubrik { get; set; }
+
+        public Podd() { 
+        }
+        public Podd(string id, string rubrik) 
+        {   
+            Id = id;
+            Rubrik = rubrik;
+    }
+    }
+}
+
