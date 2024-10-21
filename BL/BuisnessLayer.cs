97dc033cbcda7422aef33a17d8409155f877cb3e
ozansoyal <ozan_soyal@hotmail.com>
2024-10-21 11:22:16 +0200

    la till business layer

--- BL/BuisnessLayer.cs
+++ BL/BuisnessLayer.cs
@@ -1,7 +1,47 @@
-﻿namespace BL
+﻿using Datalagring;
+using Models;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace BL
 {
-    public class Class1
+    /// <summary>
+    /// Klass som med hjälp av andra klasser och XML hämtar själva datan från RSS och sparar det i 
+    /// en repository.
+    /// </summary>
+    public class PoddController
     {
+        private PoddRepository poddRepository;
+
+        public PoddController()
+        {
+            //konstruera en PoddRepository
+            poddRepository = new PoddRepository();
+        }
+
+        public List<Podd> HämtaAllaPoddar()
+        {
+            return poddRepository.HämtaAllaPoddar();
+        }
+
+        public void getFromRss(string rssLink)
+        {
+            XmlReader myXmlReader = XmlReader.Create(rssLink);
+            SyndicationFeed poddFeed = SyndicationFeed.Load(myXmlReader);
+
+            foreach (SyndicationItem item in poddFeed.Items)
+            {
+                Podd aPod = new Podd
+                {
+                    Id = item.Id.ToString(),
+                    Rubrik = item.Title.Text
+
+                    //dataGridView.Rows.Add(item.Title.Text, item.Links[0].Uri.ToString());     eller liknande
+
+                };
+                poddRepository.LäggTillPodd(aPod);
+            }
+        }
 
     }
 }
---
+++ Models/Avsnitt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+   
+
+namespace Models
+{
+    public class Avsnitt
+    {
+        public string Titel { get; set; }
+        public string Beskrivning { get; set; } 
+       
+    }
+}
