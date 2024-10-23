78c4581669242b2dc06895ede4a02b3e980e914b
Juanka <168719051+JoanMW2@users.noreply.github.com>
2024-10-23 14:45:16 +0200

    interface

--- Models/IEpisode.cs
+++ Models/IEpisode.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-   
 
 namespace Models
 {
-    [Serializable]
-    public class Episode: IEpisode
+    public interface IEpisode
     {
-       
+
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTimeOffset PublishDate { get; set; }
@@ -18,15 +16,6 @@ namespace Models
         public string Duration { get; set; }
 
         public string Link { get; set; }
-        public Episode(string title, string description, DateTimeOffset publishDate, string link, string duration)
-        { 
-            
-            Title = title;
-            Description = description;
-            PublishDate = publishDate;
-            Link = link;
-            Duration = duration;
-        }
+       
     }
-
 }
---
+++ Models/IPodcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public interface IPodcast
+    {
+        string Title { get; set; }
+        string Name { get; set; }
+        string Category { get; set; }
+        public List<IEpisode> Episode { get; set; }
+        int Count { get; set; }
+    }
+}
